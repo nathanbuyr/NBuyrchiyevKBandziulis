8d1ed0721b61278bfdc66b5f0c89fb1400e51d3a
KarolisBz <G00417529@atu.ie>
2023-04-03 19:22:05 +0100

    added new css and js

--- script.js
+++ script.js
@@ -2,4 +2,18 @@
 function darkMode()
 {
     document.body.style.backgroundColor = "black";
+
+    // changing all p elements and h1 elements to white
+    let textElements = document.getElementsByTagName("p");
+    let titleElements = document.getElementsByTagName("h1");
+
+    for(let i = 0; i < textElements.length; i++)
+    {
+        textElements[i].style.color = "white";
+    }
+
+    for(let i = 0; i < titleElements.length; i++)
+    {
+        titleElements[i].style.color = "white";
+    }
 }
